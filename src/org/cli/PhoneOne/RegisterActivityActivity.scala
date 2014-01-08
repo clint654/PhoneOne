7ea0dd422a8b4f2a702643fe1048ae75545d1ce6
Clinton Bentley <clint@connection-telecom.com>
2014-01-08 11:22:59 +0200

    inintal project version

---
+++ src/org/cli/PhoneOne/RegisterActivityActivity.scala
@@ -0,0 +1,15 @@
+package org.cli.PhoneOne
+
+import android.os.Bundle
+import org.scaloid.common.SActivity
+
+class RegisterAccountActivity extends SActivity {
+  /**
+   * Called when the activity is first created.
+   */
+  override def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+    
+  }
+}
+
